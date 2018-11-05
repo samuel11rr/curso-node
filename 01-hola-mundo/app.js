366ad440916d3fadb00e8a1d8f94b70d75fd10c8
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-11-05 17:38:45 -0600

    Initial commit

---
+++ 01-hola-mundo/app.js
@@ -0,0 +1,5 @@
+let nombre = 'Samus';
+
+// console.log('Hola '+ nombre);
+
+console.log(`Hola ${ nombre }`);
---
+++ 01-hola-mundo/app3.js
@@ -0,0 +1,15 @@
+console.log('Inicio');
+
+setTimeout( function(){
+    console.log('Primer timeout');
+}, 3000 );
+
+setTimeout( function(){
+    console.log('Segundo timeout');
+}, 0 );
+
+setTimeout( function(){
+    console.log('Tercer timeout');
+}, 0 );
+
+console.log('Fin del programa');
