992a93c224f45aa3f4f21be92d0fe114911e7ce7
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-11-14 10:34:10 -0600

    ESCONDEMOS APIKEY DE GOOGLE

--- 05-clima-mundo/clima/clima.js
+++ 05-clima-mundo/clima/clima.js
@@ -1,8 +1,9 @@
-const axios = require('axios');
+const axios   = require('axios');
+const apiKey  = require('../config/apiKey.json')[0].apiKey;
 
 const getClima = async(lat, lng) => {
 
-  let resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&units=metric&appid=2329fa8399bff48bcc67973ba7d8929b`)
+  let resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&units=metric&appid=${ apiKey }`)
 
   return resp.data.main.temp;
 }
