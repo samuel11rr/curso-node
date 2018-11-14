ee39e363cd04cb83f47abdfbc27811390fa191d2
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-11-14 12:14:30 -0600

    CREANDO VISTAS CON HBS

---
+++ 06-webserver/hbs/helpers.js
@@ -0,0 +1,16 @@
+//helpers HBS
+
+const hbs = require('hbs');
+
+hbs.registerHelper('getAnio', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('capitalizar', ( texto ) => {
+  let palabras = texto.split(' ');
+  palabras.forEach( (palabra, idx) => {
+    palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+  });
+
+  return palabras.join(' ');
+});
--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,20 +1,26 @@
 const express = require('express');
-const app = express();
+const app     = express();
+const hbs     = require('hbs');
+
+require('./hbs/helpers');
 
 app.use( express.static( __dirname + '/public' ) );
 
+//Express HBS Engine
+hbs.registerPartials( __dirname + '/views/parciales' );
+app.set('view engine', 'hbs');
+
 
 app.get('/', (req, res) => {
-  // res.send('Hello World');
-
-  let salida = {
-    nombre: 'Samuel',
-    edad: 27,
-    url: req.url
-  };
-
-  res.send(salida);
+  res.render('home', {
+    nombre: 'samuel RAMIREZ'
+  });
 })
+
+app.get('/about', (req, res) => {
+  res.render('about');
+})
+
 
 app.listen(3000, () => {
   console.log('Escuchando peticiones en puerto 3000');
