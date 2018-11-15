9fab2d6dd8a2bed5ef528c3790e534c0e83ebd00
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-11-15 11:01:30 -0600

    RUTAS ESTABLECIDAS
    Otras configuraciones

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,9 +1,48 @@
-const express = require('express')
-const app = express()
+require('./config/config');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+//midleware
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json());
+
 
 app.get('/', function (req, res) {
-  res.json('Hello World')
-})
+  res.json('Hello World');
+});
+
+app.get('/usuario', function (req, res) {
+  res.json('GET Usuario');
+});
+
+app.post('/usuario', function (req, res) {
+  let body = req.body;
+
+  if ( body.nombre === undefined ) {
+    res.status(400).json({
+      ok: false,
+      mensaje: 'El nombre es necesario'
+    });
+  } else {
+    res.json({
+      persona: body
+    });
+  }
+
+});
+
+app.put('/usuario/:id', function (req, res) {
+  let id = req.params.id;
+
+  res.json({id});
+});
+
+app.delete('/usuario', function (req, res) {
+  res.json('DELETE Usuario');
+});
+
 
 app.listen(3000, () => {
   console.log('Escuchando puerto ', 3000);
