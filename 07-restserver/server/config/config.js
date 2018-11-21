54f09d499846aff16f1a0ca49716233d04807ab4
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-11-21 12:43:17 -0600

    OCULTAMOS CREDENCIALES
    heroku config:set MONGO_URI="XXXXXXX"
    
    heroku config:get nombre
    heroku config:unset nombre
    heroku config:set nombre="Algo"

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else{
-  urlDB = 'mongodb://cafe-user:abc123@ds249737.mlab.com:49737/cafe';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
