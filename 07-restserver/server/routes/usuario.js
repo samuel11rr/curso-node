0878b1075c4f8e50a381403692a42d3c6a622419
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-11-21 10:21:30 -0600

    VALIDACIONES AL ACTUALIZAR DATOS

--- 07-restserver/server/routes/usuario.js
+++ 07-restserver/server/routes/usuario.js
@@ -1,5 +1,6 @@
 const express   = require('express');
 const bcrypt    = require('bcrypt');
+const _         = require('underscore');
 const Usuario   = require('../models/usuario');
 const app       = express();
 
@@ -32,24 +33,13 @@ app.post('/usuario', function (req, res) {
     })
 
   });
-
-  // if ( body.nombre === undefined ) {
-  //   res.status(400).json({
-  //     ok: false,
-  //     mensaje: 'El nombre es necesario'
-  //   });
-  // } else {
-  //   res.json({
-  //     persona: body
-  //   });
-  // }
 });
 
 app.put('/usuario/:id', function (req, res) {
   let id = req.params.id;
-  let body = req.body;
+  let body = _.pick( req.body, ['nombre', 'email', 'img', 'role', 'estado'] ) ;
 
-  Usuario.findByIdAndUpdate( id, body, {new:true}, (err, usuarioDB) => {
+  Usuario.findByIdAndUpdate( id, body, {new:true, runValidators:true}, (err, usuarioDB) => {
     if ( err ) {
       return res.status(400).json({
         ok:false,
