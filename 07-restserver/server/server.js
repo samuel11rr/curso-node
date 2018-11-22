c2fc40fee1e4dbdc32ac9a69e8652c2cb0907b2f
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-11-22 17:38:07 -0600

    AGRUPACION DE RUTAS

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -12,7 +12,8 @@ app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
 
 
-app.use( require('./routes/usuario') );
+// Configuracion global de rutas
+app.use( require('./routes/index') );
 
 
 mongoose.connect( process.env.URLDB, (err, res) => {
