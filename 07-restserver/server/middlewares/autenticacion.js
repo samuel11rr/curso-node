b0dccc538dc2c244567f01bef0884064eadbc671
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-12-05 15:50:00 -0600

    SUBIDA DE ARCHIVOS

--- 07-restserver/server/middlewares/autenticacion.js
+++ 07-restserver/server/middlewares/autenticacion.js
@@ -42,7 +42,31 @@ let verificaAdmin_Role = ( req, res, next ) => {
 
 
 
+// VERIFICA TOKEN PARA IMAGEN
+let verificaTokenImg = ( req, res, next ) => {
+    let token = req.query.token;
+
+    jwt.verify( token, process.env.SEED, (err, decoded) =>{
+        if (err) {
+          return res.status(401).json({
+            ok: false,
+            err: {
+              message: 'Token no válido'
+            }
+          });
+        }
+
+        req.usuario = decoded.usuario;
+
+        next();
+    });
+
+}
+
+
+
 module.exports = {
   verificaToken,
-  verificaAdmin_Role
+  verificaAdmin_Role,
+  verificaTokenImg
 }
--- 07-restserver/server/routes/index.js
+++ 07-restserver/server/routes/index.js
@@ -5,6 +5,8 @@ app.use( require('./usuario') );
 app.use( require('./login') );
 app.use( require('./categoria') );
 app.use( require('./producto') );
+app.use( require('./upload') );
+app.use( require('./imagenes') );
 
 
 module.exports = app;
