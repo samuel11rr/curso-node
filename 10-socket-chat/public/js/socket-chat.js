3314499bf452438762f942c2b82a29bc882dce89
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-03-14 14:31:32 -0600

    ACTUALIZACION DEL FRONTEND

--- 10-socket-chat/public/js/socket-chat.js
+++ 10-socket-chat/public/js/socket-chat.js
@@ -1,24 +1,26 @@
 var socket = io();
 
-var params = new URLSearchParams( window.location.search );
+var params = new URLSearchParams(window.location.search);
 
-if ( !params.has('nombre') || !params.has('sala') ) {
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new error('El nombre y sala son necesarios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
-  nombre: params.get('nombre'),
-  sala: params.get('sala')
-}
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
 
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function(resp){
+    socket.emit('entrarChat', usuario, function(resp) {
         console.log('Usuarios conectados', resp);
     });
+
 });
 
 // escuchar
@@ -30,8 +32,8 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-// socket.emit('enviarMensaje', {
-//     usuario: 'Fernando',
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -39,22 +41,18 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
     console.log('Servidor:', mensaje);
-
 });
 
-
-
-// Escuchas cambios de usuarios
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
     console.log(personas);
 });
 
-
-
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('mensaje privado: ', mensaje);
-    
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
--- 10-socket-chat/server/sockets/socket.js
+++ 10-socket-chat/server/sockets/socket.js
@@ -1,55 +1,42 @@
-const { io }            = require('../server');
-const { Usuarios }      = require('../classes/usuarios');
-const { crearMensaje }  = require('../utils/utils');
-
-const usuarios = new Usuarios();
+const { io } = require('../server');
 
 
-io.on('connection', ( client ) => {
+io.on('connection', (client) => {
 
-    client.on('entrarChat', ( data, callback ) => {
+    console.log('Usuario conectado');
 
-        if ( !data.nombre || !data.sala ) {
-            return callback({
-              error: true,
-              mensaje: 'El nombre/sala es necesario'
-            });
-        }
-
-        client.join(data.sala);
-
-        let personas = usuarios.agregarPersona( client.id, data.nombre, data.sala );
-
-        // client.broadcast.emit('listaPersona', usuarios.getPersonas());
-        client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala( data.sala ));
-
-        callback( personas );
+    client.emit('enviarMensaje', {
+        usuario: 'Administrador',
+        mensaje: 'Bienvenido a esta aplicación'
     });
-
-
-    client.on('crearMensaje', (data) => {
-        let persona = usuarios.getPersona( client.id );
-
-        let mensaje = crearMensaje( persona.nombre, data.mensaje );
-
-        client.broadcast.to(persona.sala).emit( 'crearMensaje', mensaje );
-    })
 
 
 
     client.on('disconnect', () => {
-        let personaEliminada = usuarios.borrarPersona( client.id );
-
-        client.broadcast.to(personaEliminada.sala).emit('crearMensaje', crearMensaje( 'Administrador', `${ personaEliminada.nombre } salió` ));
-        client.broadcast.to(personaEliminada.sala).emit('listaPersona', usuarios.getPersonasPorSala( personaEliminada.sala ));
+        console.log('Usuario desconectado');
     });
 
+    // Escuchar el cliente
+    client.on('enviarMensaje', (data, callback) => {
 
-    // MENSAJES PRIVADOS
-    client.on('mensajePrivado', data => {
-        let persona = usuarios.getPersona( client.id );
+        console.log(data);
 
-        // **** tenemos que validar data
-        client.broadcast.to( data.para ).emit( 'mensajePrivado', crearMensaje(  persona.nombre, data.mensaje) );
+        client.broadcast.emit('enviarMensaje', data);
+
+
+        // if (mensaje.usuario) {
+        //     callback({
+        //         resp: 'TODO SALIO BIEN!'
+        //     });
+
+        // } else {
+        //     callback({
+        //         resp: 'TODO SALIO MAL!!!!!!!!'
+        //     });
+        // }
+
+
+
     });
+
 });
